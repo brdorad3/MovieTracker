ad49e930a62e55050269f148fa9321c43c027416
brdorad3 <vunicadis@gmail.com>
2024-11-12 19:19:36 +0100

    navbar

--- client/src/navbar.tsx
+++ client/src/navbar.tsx
@@ -1,11 +1,17 @@
-
+import { Search, SunMoon } from "lucide-react"
 
  const Navbar  = () => {
 
     return(
         <>
-        <nav className="h-[8%] w-screen bg-black text-red-800 grid grid-cols-custom justify-center gap-x-[20px] items-center">
-            <h1 className="text-2xl">Robnite</h1>
+        <nav className="h-[8%] w-screen bg-first  grid grid-cols-custom justify-center gap-x-[20px] items-center">
+            <h1 className="text-3xl text-acc">Robnite</h1>
+            <p className="col-start-3 text-sec">MOVIES</p>
+            <p className="col-start-4 text-sec">TV SHOWS</p>
+            <p className="col-start-5 text-sec">NEWS</p>
+            <Search className="col-start-10 justify-self-end text-sec" size={30} />
+            <SunMoon className="col-start-11 text-sec justify-self-center" size={30} />
+            <div className="w-[40px] h-[40px] rounded-full bg-sec col-start-12"></div>
         </nav>
         
         </>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,7 +11,33 @@ export default {
         '16': 'repeat(16, minmax(0, 1fr))',
 
         'custom': 'repeat(12, 90px)'
+      },
+      colors: {
+        "first": "#222222",
+        "acc": "#c03221",
+        'sec': '#f7f7ff',
+        'vour': '#f2d0a4'
       }
+     /*   colors: {
+          "first": "#0d1f2d",
+          "acc": "#dd614a",
+          'sec': '#f5f2b8',
+          'vour': '#f2d0a4'
+        }
+          */
+       /* colors: {
+          "first": "#22223b",
+          "acc": "#92dce5",
+          'sec': '#eee5e9',
+          'vour': '#f2d0a4'
+        }
+          */
+       /* colors: {
+          "first": "#0f1020",
+          "acc": "#e3b23c",
+          'sec': '#edebd7',
+          'vour': '#f2d0a4'
+        }*/
     },
   },
   plugins: [],
