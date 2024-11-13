e4cc40a9ab86035a4b39e850c49943c12f0b8594
brdorad3 <vunicadis@gmail.com>
2024-11-13 15:39:42 +0100

    hero

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Navbar from './navbar'
+import Hero from './hero'
 
 function App() {
  
@@ -8,6 +9,7 @@ function App() {
     <>
     <div className='w-screen h-full'>
     <Navbar/>
+    <Hero/>
     </div>
   
     </>
---
+++ client/src/hero.tsx
@@ -0,0 +1,17 @@
+
+
+const Hero = () => {
+
+    return(
+        <>
+        <div className="w-screen h-[92%] n grid grid-cols-custom gap-x-[20px] justify-center grid-rows-12">
+            <span className="s col-start-1 row-start-4 text-8xl whitespace-nowrap text-black">The Batman</span>
+            <span className="fade-effect1 row-start-6"></span>
+            <q className="row-start-7 whitespace-nowrap b text-xl">They think I am hiding in the shadows, but I am the shadows.</q>
+            <p className="row-start-8 text-[10px] whitespace-nowrap">1 MARCH 2022</p>
+        </div>
+        </>
+    )
+}
+
+export default Hero
