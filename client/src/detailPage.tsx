279b01564293f6e0e4cfb115c1340d3467c79411
brdorad3 <vunicadis@gmail.com>
2024-11-27 14:43:57 +0100

    hero

--- client/src/detailPage.tsx
+++ client/src/detailPage.tsx
@@ -1,6 +1,7 @@
 import { useLocation } from "react-router-dom"
 import axios from "axios"
 import { useState, useEffect } from "react"
+import { PlayCircle } from "lucide-react"
 
 interface info{
     backdrop_path: string,
@@ -34,25 +35,42 @@ console.log(detailedInfo)
         <>
         {
             detailedInfo &&
-<div className="w-screen h-full relative bg-cover grid grid-cols-custom grid-rows-12 justify-center"
-        style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${detailedInfo.backdrop_path})`}}
-        >
-            
-         <div className="flex justify-center gap-20 row-start-2 text-white col-start-6">
-            <h2 className="">MOVIES</h2>
-            <h2 className="whitespace-nowrap">TV SHOWS</h2>
-            <h2>NEWS</h2>
-         </div>
-         <div className="row-start-6">
-            <h1 className="text-white lemon text-[100px] tracking-wider">{info.original_name}</h1>
-            {detailedInfo.genres &&
-            detailedInfo.genres.map((item: any)=>(
-                <p>{item.name}</p>
-            ))
-            }
-         </div>
-    
-        </div>
+<div
+  className="w-screen h-full relative bg-cover grid grid-cols-custom grid-rows-12 justify-center"
+  style={{
+    backgroundImage: `url(https://image.tmdb.org/t/p/original${detailedInfo.backdrop_path})`,
+  }}
+>
+  <div className="flex justify-center gap-20 row-start-2 text-white col-start-6">
+    <h2 className="">MOVIES</h2>
+    <h2 className="whitespace-nowrap">TV SHOWS</h2>
+    <h2>NEWS</h2>
+  </div>
+  <div className="row-start-5 flex flex-col gap-10 col-start-1 relative">
+    <div className="flex flex-col col-span-2">
+      <h1 className="text-white lemon text-[90px] tracking-wider whitespace-nowrap">
+        {detailedInfo.title}
+      </h1>
+      <ul className="flex gap-5">
+        {detailedInfo.genres &&
+          detailedInfo.genres.map((item: any) => (
+            <li
+              className="text-white whitespace-nowrap b text-lg"
+              key={item.id}
+            >
+              {item.name}
+            </li>
+          ))}
+      </ul>
+    </div>
+    <button
+      className="bg-acc px-6 py-4 rounded-full flex gap-5 whitespace-nowrap absolute top-[220px] b text-xl font-bold items-center"
+    >
+      Watch Trailer <PlayCircle></PlayCircle>
+    </button>
+  </div>
+</div>
+
         
         }
         </>
