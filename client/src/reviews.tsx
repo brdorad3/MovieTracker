d94f5d0117e6d7ad9aad66610b73c1f34a199045
brdorad3 <vunicadis@gmail.com>
2024-12-22 10:08:08 +0100

    reviews

--- client/src/reviews.tsx
+++ client/src/reviews.tsx
@@ -1,16 +1,17 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { DateTime } from "luxon"
 
 const Reviews = (props:any) => {
 
     const info = props.state
-    const [photo, setPhoto] = useState<any[]>([])
+    const [review, setReview] = useState<any[]>([])
 
     const fetchImages = async() => {
         try{
             const response = await axios.post(`${import.meta.env.VITE_API}/reviews`, {info})
             console.log(response.data)
-            setPhoto(response.data.backdrops)
+            setReview(response.data.results)
         }catch(e){
             console.error(e)
         }
@@ -22,7 +23,26 @@ const Reviews = (props:any) => {
 
     return(
         <>
-       
+            <div className="flex flex-col gap-5">
+                {review &&
+                review.map((item: any) => (
+                    <div className="p-4 shad rounded-md flex flex-col gap-5">
+                        <div className="flex items-center gap-3">
+                        <div className="bg-black w-12 h-12 rounded-full"></div>
+                        <div>
+                        <p>{item.author}</p>
+                        <div className="flex gap-2">
+                        {item.author_details.rating &&
+                            <p>{item.author_details.rating}/10</p>
+                        }
+                        <p>Created on {DateTime.fromISO(item.created_at).toLocaleString(DateTime.DATE_MED)}</p>
+                        </div>
+                        </div>
+                        </div>
+                        <p>{item.content}</p>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
